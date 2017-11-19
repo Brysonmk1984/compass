7c83f7974f9167a0f4dbfe7f0be6c300bdf15145
Bryson Kruk <brysonmk1984@gmail.com>
2017-11-19 09:37:52 -0700

    test

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React from 'react';
 
 export default class App extends Component {
-            testMethod(){
-                console.log(
-                    'dumb test'
-                )
-                
-                
-            }
+testMethod(){
+    console.log(
+        'dumb test'
+    )
+    
+    
+}
+
     render () {
         return <p>Thiasdds is my new reacts app</p>
     }
