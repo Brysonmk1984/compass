e667bf429afb2165eda095e30c16cac396def0a4
Bryson Kruk <brysonmk1984@gmail.com>
2017-11-19 09:31:46 -0700

    tttt

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import React from 'react';
 export default class App extends Component {
             testMethod(){
                 console.log(
-
                     'dumb test'
                 )
                 
