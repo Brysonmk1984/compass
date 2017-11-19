13514470a4417027980f125192b448e8f6f49c26
Bryson Kruk <brysonmk1984@gmail.com>
2017-11-19 09:30:49 -0700

    tes2t

--- src/App.js
+++ src/App.js
@@ -3,9 +3,10 @@ import React from 'react';
 export default class App extends Component {
             testMethod(){
                 console.log(
-                    
+
                     'dumb test'
                 )
+                
             }
     render () {
         return <p>Thiasdds is my new reacts app</p>
